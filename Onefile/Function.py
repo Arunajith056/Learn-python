d95c89d61b21a523326408250107bc79ae53a2c5
Arunajith056 <arunajith056@gmail.com>
2023-12-25 15:34:15 +0530

    functions added

---
+++ Onefile/Function.py
@@ -0,0 +1,52 @@
+# functions are block of reusable code is designed perform a specific task 
+# parameter and arguments
+#variable scope- local variable and global variable
+#variable length arguments (*args)-store in tuple 
+    #keyword arguments - (**kwargs)- store in dictionary 
+# passing list and dictionary of arguments 
+# return the list and dictionary 
+# ''' using for documention for functions is known as Docstring'''
+greet_all ="Welcome"  #globle variable
+def greet(name):#name variable for this function is parameter 
+	'''Function get name from user and print the name'''
+	print(name) 
+greet("Robin hood")#passing the value (arguments)-positional arguments
+
+# variable scope
+def greet1(n):
+	greet2_all="Hello everyone" #local variable only access in the function
+	'''change the global variable in local using global keyword'''
+	global greet_all 
+	greet_all = "Changed_Welcome"
+	print(n)
+	print(greet2_all)
+greet1(greet_all)# assign global variable to the arguments
+#print(greet2_all) - try it gives the nameerror
+print(greet_all)
+
+# Default parameters
+def greet_default(name, age=24): #give value by default is the default parameters
+	''' ONLY give the arguments to the name , age is not required but u give also'''
+	print(f'your name is {name} and your age is  { age }')
+greet_default("Ajith") # passing one arguments error will not come 
+
+# keyword arguments 
+def greet_keyword(name, age):
+	''' Assign the parameter fo value in arguments '''
+	print(f'by keyword your name is {name} and your age is {age}')
+greet_keyword(age=24, name="Ajith")
+
+#return Something in the function as required 
+def sum(a,b):
+	return a+b 
+print(sum(2,3))
+
+''' variable length Arguments '''
+# *args (Arbitrary Positional Arguments):
+# Allows a function to accept a variable number of positional arguments.
+def variable_n(*args):
+	num = 0
+	for i in args:
+		num += i
+	return num 
+print(variable_n(1,2,3,4,5,))
